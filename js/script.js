3addfc0d5d8c72a396dd4c227c649dc923e97633
austin <124389297+austin-scavone@users.noreply.github.com>
2023-04-27 16:41:18 +0000

    fixes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Mr Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Mr Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+'use strict'
+
+/**
+ * This function uses a selection component from https://github.com/CreativeIT/getmdl-select
+ */
+
+function myButtonClicked() {
+  // input
+  const numberGuessed = parseInt(document.getElementById('guessed-number').value)
+
+  // process
+  //pass
+
+  // output
+  document.getElementById('answer').innerHTML = numberGuessed
+}
